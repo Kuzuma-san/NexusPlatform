afc394635f797167092e03f279ef3ec0b4b0a086
Harshit Singh Bisht <harshitsinghbisht@Mohits-MacBook-Air.local>
2026-03-04 23:15:48 +0530

    Making UserModel and DTO

--- apps/nexus-platform/src/app/app.module.ts
+++ apps/nexus-platform/src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import  { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'nexus_db',
+      synchronize: true,
+      models: [User]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/nexus-platform/src/users/entities/user.entity.ts
+++ apps/nexus-platform/src/users/entities/user.entity.ts
@@ -1,1 +1,32 @@
-export class User {}
+import { Model, Table, Column, DataType } from "sequelize-typescript";
+
+@Table({tableName: 'users'})
+export class User extends Model<User>{
+    @Column({
+        type: DataType.STRING,
+    })
+    username: string;
+
+    @Column({
+        type: DataType.STRING
+    })
+    email: string;
+
+    @Column({
+        type: DataType.STRING,
+    })
+    password: string;
+
+    @Column({
+        type: DataType.BOOLEAN,
+        defaultValue: true,
+    })
+    isActive: boolean;
+
+    @Column({
+        type: DataType.BOOLEAN,
+        defaultValue: false,
+    })
+    isVerified: boolean;
+
+}
